0b93a751fda775a403c0ee6fe9af76e094cef06f
sam20code <56734174+sam20code@users.noreply.github.com>
2021-05-16 18:21:40 +0530

    Add files via upload

---
+++ DataFiles/scripts/ChangeScene.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangeScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void nextscene1()
+    {
+        SceneManager.LoadScene("Info");
+    }
+   public void Video()
+    {
+        SceneManager.LoadScene("Video");
+    }
+    public void final()
+    {
+        SceneManager.LoadScene("CerebroThankYou");
+    }
+    public void back()
+    {
+        SceneManager.LoadScene("Info");
+    }
+
+}
